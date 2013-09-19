bb2edaab9b907f906589e98f6d998803e523b687
matt <matt@new-host-2.home>
2013-09-18 21:00:56 -0400

    Initial commit

---
+++ jquery.mutation-listener.js
@@ -0,0 +1,99 @@
+(function ($) {
+  var MutationObserver = window.MutationObserver || window.WebKitMutationObserver || window.MozMutationObserver,
+      observers = [];
+
+  $.fn.mm_add_mutation_listner = function (target_class_selector, callback_event_name) {
+
+    return this.each(function(){
+      if (typeof (MutationObserver) == 'undefined') {
+        var config = {
+              attributes: true,
+              childList: true,
+              characterData: true
+            };
+
+            context = $(this).get(0);
+            //Convert css selector
+            target_class_selector = target_class_selector.split('.').join(' ').trim();
+
+        callback = function (mutations) {
+          $.each(mutations, function (index, mutation) {
+            switch (mutation.type) {
+              case 'childList':
+                addedNodes_callback(mutation.addedNodes);
+                break;
+              case 'characterData':
+                //
+                break;
+            }
+          });
+        },
+
+        addedNodes_callback = function (addedNodes) {
+          /**
+           * Logic added to proplerly match css selector
+           * Will match "matt" in "matt class" or "class matt", but not "matt2" in "matt class", or "matt class" in "matt"
+           */
+          var target_class_array = target_class_selector.split(' '),
+              node_class_array = '',
+              class_selectors_not_matching = target_class_array.length;
+
+          $.each(addedNodes, function (index, addedNode) {
+            if (typeof(addedNode.className) !== 'undefined') {
+              node_class_array = addedNode.className.split(' ');
+
+              $.each(node_class_array, function(i) {
+                var node_class = node_class_array[i];
+                $.each(target_class_array, function(i2) {
+                  var target_class = target_class_array[i2];
+                  if(node_class === target_class) {
+                    class_selectors_not_matching--;
+                  }
+                }); 
+              });
+              
+              if(class_selectors_not_matching === 0) {
+                $(document).trigger(callback_event_name);
+              }
+            }
+          });
+        };
+
+        observers.push(new MutationObserver(callback).observe(context, config));
+
+      } else if (typeof (MutationEvent) !== 'undefined') {
+        $(this).on('DOMNodeInserted', function (e) {
+          var fn = arguments.callee;
+          if ($(target_class_selector).length > 0) {
+            $(document).trigger(callback_event_name);
+            $(this).off('DOMNodeInserted', fn);
+          }
+        });
+      } else {
+        throw('Web Browser does not support MutationObserver API or MutationEvent API');
+      }
+    });
+  };
+})(jQuery);
+
+/*
+Test code
+*/
+(function($) {
+  $('body').mm_add_mutation_listner('.class.matt', 'matt_class_added');
+  
+  $('body').mm_add_mutation_listner('.matt2', 'matt2_class_added');
+
+  $(document).on('matt_class_added', function(e){
+    console.log('matt div added');
+  });
+  
+  $(document).on('matt2_class_added', function(e){
+    console.log('matt2 div added');
+  });
+
+  $(function() {
+    $('body').append('<div class="matt class"></div>');
+    $('body').append('<div class="matt2 class"></div>');
+  });
+})(jQuery);
